04872ac06122bfcaca821b7067baa6d28aeaf615
gabrielmulleer <emaildegabrielm@gmail.com>
2024-08-15 18:42:33 -0300

    feat: connect the form to the service to add new tasks to the list.

--- src/app/add-task/add-task.component.ts
+++ src/app/add-task/add-task.component.ts
@@ -1,12 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, ReactiveFormsModule } from '@angular/forms';
+import { TaskService } from '../task.service'; // Importe o serviço
 
 @Component({
   selector: 'app-add-task',
   standalone: true,
   imports: [ReactiveFormsModule],
   templateUrl: './add-task.component.html',
-  styleUrl: './add-task.component.css',
+  styleUrls: ['./add-task.component.css'],
 })
 export class AddTaskComponent implements OnInit {
   taskForm: FormGroup = new FormGroup({
@@ -16,18 +17,23 @@ export class AddTaskComponent implements OnInit {
   });
 
   private nextId: number = 1;
+
+  constructor(private taskService: TaskService) {} // Injeção do serviço
+
   ngOnInit() {}
+
   onSubmit() {
     if (this.taskForm.valid) {
       const newTask = {
         ...this.taskForm.value,
         id: this.nextId++,
       };
-      console.warn(newTask);
+      this.taskService.addTask(newTask); // Adiciona a tarefa ao serviço
 
       this.resetForm();
     }
   }
+
   resetForm() {
     this.taskForm.reset({
       id: '',
