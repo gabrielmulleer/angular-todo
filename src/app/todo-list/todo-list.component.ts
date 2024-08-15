240f5b422aa551b643abaa3604112bea4b699c0e
gabrielmulleer <emaildegabrielm@gmail.com>
2024-08-15 18:42:48 -0300

    feat: inject the service and use it to display the tasks in the interface

---
+++ src/app/todo-list/todo-list.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { Task, TaskService } from '../task.service';
+
+@Component({
+  selector: 'app-todo-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './todo-list.component.html',
+  styleUrl: './todo-list.component.css',
+})
+export class TodoListComponent implements OnInit {
+  tasks: Task[] = [];
+
+  constructor(private taskService: TaskService) {}
+
+  ngOnInit(): void {
+    this.tasks = this.taskService.getTasks();
+  }
+}
